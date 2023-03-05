03c6b20bdd601b3b0e567bd4f50f27d425bbac92
Evil559 <122262267+Evil559@users.noreply.github.com>
2023-03-05 19:22:55 +0200

    send telegram/discord log

--- config.js
+++ config.js
@@ -1,3 +1,7 @@
 run({
-  webhook: 'https://discord.com/api/webhooks/...'
+  platform: 'discord',
+  chatId: '123456789',
+  botToken: '1234567890:AAAAAAAAAAAAAAAAABBBBBBBBBBBBBBBBBB',
+  webhookUrl: 'https://discord.com/api/webhooks/...'
 })
+// platform: telegram/discord
